e515a7e45ff0b1b6311ce16bcc6d0cf857883199
prabhatkatoch <prabhatkatoch@PRABHATKATO2C57>
2023-01-24 13:52:38 -0600

    Add project files.

---
+++ sqlazure/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlazure.Models;
+using sqlapp.Services;
+namespace sqlazure.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+
+        ProductService productService = new ProductService();
+           Products= productService.GetProducts();
+        }
+    }
+}
